7bb67e06f310541ad1b5ef0442b3a3e6c5b9efe3
Payal Priyadarshini <80968938+Payal3214@users.noreply.github.com>
2026-01-05 22:56:43 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
