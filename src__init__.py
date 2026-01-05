0affb0119e357854df458d675e8410ed1067a777
Payal Priyadarshini <80968938+Payal3214@users.noreply.github.com>
2026-01-05 16:44:40 +0530

    Initialize project structure in src/__init__.py
    Added project structure and initial files.

--- src__init__.py
+++ src__init__.py
@@ -1,1 +1,12 @@
 
+ai-product-review-analyzer-with-rag-system/
+│
+├ app.py
+├ requirements.txt
+├ setup.py
+├ src/
+│   ├ __init__.py
+│   ├ rag.py
+│   ├ cleaner.py
+│   ├ embeddings.py
+│   └ ...
