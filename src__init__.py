e60fe7b7ff24743050b56083072f4cc30a4dfa9f
Payal Priyadarshini <80968938+Payal3214@users.noreply.github.com>
2026-01-05 16:56:20 +0530

    Update project structure in __init__.py
    Clarified structure by marking 'setup.py' as a file and 'src/' as a folder.

--- src__init__.py
+++ src__init__.py
@@ -1,9 +1,9 @@
-
 ai-product-review-analyzer-with-rag-system/
 │
 ├ app.py
 ├ requirements.txt
-├ setup.py
+├ setup.py        ← this must be a FILE
+├ src/            ← this must be a FOLDER
 │   ├ __init__.py
 │   ├ rag.py
 │   ├ cleaner.py
